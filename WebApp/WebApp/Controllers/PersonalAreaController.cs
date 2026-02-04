e4970d5fd0c52820324fe415c050246c2a2f20ee
tavez38 <tavecchia.lorenzo.22@itisriva.edu.it>
2026-02-04 15:48:54 +0100

    logica back end area personale
    implementazione della logica back end per il recupero dei progetti dall'arraylist che li contiene

---
+++ WebApp/WebApp/Controllers/PersonalAreaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [Route("/login/personalArea")]
+    [ApiController]
+    public class PersonalAreaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllWorks()
+        {
+            var progetti = ProgettiUtente.progetti;
+            return Ok(progetti);
+        }
+    }
+}
