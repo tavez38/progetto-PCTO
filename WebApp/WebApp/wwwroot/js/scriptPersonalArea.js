3d7c319cf883edcfac05e8d81fff063787757e18
tavez38 <tavecchia.lorenzo.22@itisriva.edu.it>
2026-02-10 09:37:09 +0100

    implementazione funzioni indipendenti per generare le tabelle

--- WebApp/WebApp/wwwroot/js/scriptPersonalArea.js
+++ WebApp/WebApp/wwwroot/js/scriptPersonalArea.js
@@ -11,12 +11,13 @@ import {
 } from "../js/utilities.js";
 
 
-document.addEventListener("DOMContentLoaded", loadWorks());
+document.addEventListener("DOMContentLoaded", loadWorks);
 document.getElementById("linkLogOutPA").addEventListener("click", logout);
 document.getElementById("btnAddWork").addEventListener("click", goToUploadWorkPage);
 document.getElementById("btnMsg").addEventListener("click", goToMessaggiPage);
-document.getElementById("sendReqChatbot").addEventListener("click", sendOllamaRequest);
+//document.getElementById("sendReqChatbot").addEventListener("click", sendOllamaRequest);
 
+var vWorks = [];
 
 async function loadWorks() {
     const token = localStorage.getItem("token");
@@ -46,42 +47,50 @@ async function loadWorks() {
             document.getElementById("tableBodyProj").innerHTML = `<tr class="rowNoProject"><td colspan="4" id="colNoProject">Nessun progetto caricato</td></tr>`;
             return;
         }
-        const fragment = document.createDocumentFragment();
-
         data.forEach(element => {
-            const tr = document.createElement("tr");
-            tr.className = "rows";
-
-            // Creazione manuale sicura delle celle
-            const tdTitle = document.createElement("td");
-            tdTitle.className = "tableTitle";
-            tdTitle.textContent = element.title;
-            tr.appendChild(tdTitle);
-
-            const tdDesc = document.createElement("td");
-            tdDesc.className = "tableDesc";
-            tdDesc.textContent = element.description;
-            tr.appendChild(tdDesc);
-
-            const tdDate = document.createElement("td");
-            tdDate.className = "tableDate";
-            tdDate.textContent = element.scadenza;
-            tr.appendChild(tdDate);
-
-            const tdHour = document.createElement("td");
-            tdHour.className = "tableHour";
-            tdHour.textContent = element.orarioScadenza;
-            tr.appendChild(tdHour);
-
-            fragment.appendChild(tr);
+            vWorks.push(element);
         });
-
-        tableBody.appendChild(fragment);
+        createWorksTable();
+        
     }
     catch (error) {
         console.log(error);
     }
 }
+
+function createWorksTable() {
+    const tableBody = document.getElementById("tableBodyProj");
+    const fragment = document.createDocumentFragment();
+    vWorks.forEach(element => {
+        const tr = document.createElement("tr");
+        tr.className = "rows";
+
+
+        const tdTitle = document.createElement("td");
+        tdTitle.className = "tableTitle";
+        tdTitle.textContent = element.title;
+        tr.appendChild(tdTitle);
+
+        const tdDesc = document.createElement("td");
+        tdDesc.className = "tableDesc";
+        tdDesc.textContent = element.description;
+        tr.appendChild(tdDesc);
+
+        const tdDate = document.createElement("td");
+        tdDate.className = "tableDate";
+        tdDate.textContent = element.scadenza;
+        tr.appendChild(tdDate);
+
+        const tdHour = document.createElement("td");
+        tdHour.className = "tableHour";
+        tdHour.textContent = element.orarioScadenza;
+        tr.appendChild(tdHour);
+
+        fragment.appendChild(tr);
+    });
+    tableBody.appendChild(fragment);
+}
+
 function sendOllamaRequest() {
     const domanda = document.getElementById("requestChatOllama").value;
     try {
